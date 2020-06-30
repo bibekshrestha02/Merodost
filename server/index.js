e1f81b3c2ad4bafbf824563329cfa8c2b4f98e11
Bibek Shrestha <shresthabbks@gmail.com>
2020-06-30 12:22:32 +0545

    success

--- server/index.js
+++ server/index.js
@@ -40,7 +40,9 @@ app.use("/contact", ContactRoute);
 app.use("/user", UserRoute);
 if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../../client", "build", "index.html")
+    );
   });
 }
 
