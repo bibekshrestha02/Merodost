8a49a4cdba9f78ec6a76aee339b36a9c494318fe
Bibek Shrestha <shresthabbks@gmail.com>
2020-06-30 12:41:14 +0545

    b

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use("/api", BlogRoute);
 app.use("/contact", ContactRoute);
 app.use("/user", UserRoute);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "build", "index.html")); // relative path
   });
