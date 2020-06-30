879e98bdacc27765c77fb278d8998a68d9da1bea
Bibek Shrestha <shresthabbks@gmail.com>
2020-06-30 12:38:04 +0545

    b

--- server/index.js
+++ server/index.js
@@ -34,15 +34,14 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 // app.use(morgan("tiny"));
-app.use(express.static("../client/build"));
+// app.use(express.static("client/build"));
 app.use("/api", BlogRoute);
 app.use("/contact", ContactRoute);
 app.use("/user", UserRoute);
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../../client", "build", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html")); // relative path
   });
 }
 
